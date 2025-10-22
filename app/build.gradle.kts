c0259b9e53b938f4365a74f9e25a92960a90eae3
stephenmcgrath20103121 <20103121@setu.ie>
2025-10-22 11:47:30 +0100

    Lab-A00 content adapted to project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    implementation(libs.timber)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
